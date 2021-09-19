d5dfda30681da24a93c9312543c4824966e00e6e
Chris Luo <chrislo5311@gmail.com>
2021-09-19 10:12:14 +0800

    重構EchoApplication及新增註解

--- src/main/java/com/infotran/springboot/linebot/controller/EchoApplication.java
+++ src/main/java/com/infotran/springboot/linebot/controller/EchoApplication.java
@@ -1,18 +1,16 @@
 package com.infotran.springboot.linebot.controller;
 
-import com.infotran.springboot.linebot.service.LineReplyMessageHandler;
+import com.infotran.springboot.linebot.service.BaseMessageHandler;
+import com.infotran.springboot.linebot.service.LineReplyMessageInterface;
 import com.infotran.springboot.linebot.service.messagehandler.HandleLocationMessageReply;
 import com.linecorp.bot.model.event.message.LocationMessageContent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.infotran.springboot.linebot.service.messagehandler.TestReplyMessageHandler;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.PostbackEvent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -22,37 +20,54 @@ import javax.annotation.Resource;
 @SpringBootApplication
 public class EchoApplication {
 
-	public static void main(String[] args) {
-	    SpringApplication.run(EchoApplication.class, args);
-	 }
-	
-	@Autowired
-	TestReplyMessageHandler replymessagehandler;
-
-	@Resource
-	LineReplyMessageHandler lineReplyMessageHandler;
-
-	@Resource
-	HandleLocationMessageReply handleLocationMessageRely;
-
-	@EventMapping
-	public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {  
-        BotApiResponse response = replymessagehandler.textMessageReply(event);
-    }
-	
-	@EventMapping
-	public void handlePostBackEvent(PostbackEvent event)throws Exception{
-		lineReplyMessageHandler.postBackReply(event);
-	}
-	
-	@EventMapping
-	public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
-		replymessagehandler.handleSticker(event.getReplyToken(), event.getMessage());
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApplication.class, args);
     }
 
+    @Resource
+    LineReplyMessageInterface lineReplyMessageHandler;
 
-	@EventMapping
-	public void handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
-		replymessagehandler.handleLocationMessageRely(event);
-	}
+    @Resource
+    BaseMessageHandler baseMessageHandler;
+
+    @Resource
+    HandleLocationMessageReply handleLocationMessageRely;
+
+    /**
+     * 處理目錄
+     * @param event PostbackEvent
+     * */
+    @EventMapping
+    public void handlePostBackEvent(PostbackEvent event) throws Exception {
+        lineReplyMessageHandler.postBackReply(event);
+    }
+
+    /**
+     * 處理使用者位置
+     * @param event MessageEvent<LocationMessageContent>
+     * */
+    @EventMapping
+    public void handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
+        handleLocationMessageRely.handleLocationMessageRely(event);
+    }
+
+    /**
+     * 測試-接收使用者文字訊息
+     * @param event MessageEvent<TextMessageContent>
+     * */
+    @EventMapping
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+        baseMessageHandler.testTextMessageReply(event);
+    }
+
+    /**
+     * 測試-接收使用者貼圖訊息
+     * @param event MessageEvent<StickerMessageContent>
+     * */
+    @EventMapping
+    public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
+        baseMessageHandler.handleSticker(event.getReplyToken(), event.getMessage());
+    }
+
+
 }
