7a6708750394a36d3aaea6a0d86478568f720f49
Chris Luo <chrislo5311@gmail.com>
2021-11-13 11:12:12 +0800

    fix Class Name Changes
    # LinebotWebMvcConfigurer - > LineBotWebMvcConfigurer
    
       b changes to uppercase

--- src/main/java/com/infotran/springboot/config/LineBotWebMvcConfigurer.java
+++ src/main/java/com/infotran/springboot/config/LineBotWebMvcConfigurer.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 @Slf4j
-public class LinebotWebMvcConfigurer implements WebMvcConfigurer {
+public class LineBotWebMvcConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
